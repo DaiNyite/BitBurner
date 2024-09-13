7abc6a9fd208b5e3389c8598047f19fd18458e64
DaiNyite <DaiNyite@gmail.com>
2024-09-13 17:47:44 -0400

    Update open.js
    - Add error message
    - Format

--- open.js
+++ open.js
@@ -1,27 +1,29 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  var server = ns.args[0];
+  if (ns.args.length > 0)
+    {var server = ns.args[0]}
+      else {ns.tprintf("ERROR: Target server not specified"); return;}
   ns.tprintf("Server Port Requiments: "+ns.getServerNumPortsRequired(server));
     if ((ns.getServerNumPortsRequired(server) >= 1)&&(ns.fileExists("BruteSSH.exe", "home"))) 
       {await ns.brutessh(server);
-      ns.tprintf("Port 1 opened");}
-        else {ns.tprintf("Port 1 ERROR");}
+      ns.tprintf("Opened Port 1");}
+        else {ns.tprintf("ERROR Port 1");}
     if ((ns.getServerNumPortsRequired(server) >= 2)&&(ns.fileExists("FTPCrack.exe", "home"))) 
       {await ns.ftpcrack(server);
-      ns.tprintf("Port 2 opened");} 
-        else {ns.tprintf("Port 2 ERROR");}
+      ns.tprintf("Opened Port 2");} 
+        else {ns.tprintf("ERROR Port 2");}
     if ((ns.getServerNumPortsRequired(server) >= 3)&&(ns.fileExists("relaySMTP.exe", "home"))) 
       {await ns.relaysmtp(server);
-      ns.tprintf("Port 3 opened");} 
-        else {ns.tprintf("Port 3 ERROR");} 
+      ns.tprintf("Opened Port 3");} 
+        else {ns.tprintf("ERROR Port 3");} 
     if ((ns.getServerNumPortsRequired(server) >= 4)&&(ns.fileExists("HTTPWorm.exe", "home"))) 
       {await ns.httpworm(server);
-      ns.tprintf("Port 4 opened");}
-        else {ns.tprintf("Port 4 ERROR");} 
+      ns.tprintf("Opened Port 4");}
+        else {ns.tprintf("ERROR Port 4");} 
     if ((ns.getServerNumPortsRequired(server) >= 5)&&(ns.fileExists("SQLInject.exe", "home"))) 
       {await ns.sqlinject(server);
-      ns.tprintf("Port 5 opened");}
-        else {ns.tprintf("Port 5 ERROR");}
+      ns.tprintf("Opened Port 5");}
+        else {ns.tprintf("ERROR Port 5");}
     if (!ns.hasRootAccess(server))
       {await ns.nuke(server);
       ns.tprintf((server)+" Nuked");}
